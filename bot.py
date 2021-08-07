62864481f1d493aacac2f35bd47fab695f04b9ee
drblobfish <jules.herrmann@gmail.com>
2021-08-07 10:13:15 +0200

    connects bot

--- bot.py
+++ bot.py
@@ -1,1 +1,14 @@
-import discord.py
+import discord
+
+import secret
+
+from discord.ext import commands
+
+client = commands.Bot(command_prefix = '²')
+
+@client.event
+async def on_ready():
+	print('Bot ready')
+
+
+client.run(secret.token)
